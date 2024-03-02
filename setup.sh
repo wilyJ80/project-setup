dcd31c1f695aafd5d31a1b8f3d9aac585b292a53
wilyJ80 <abjurandam@gmail.com>
2024-03-01 21:54:17 -0300

    Fix Python venv sourcing problem

--- setup.sh
+++ setup.sh
@@ -12,9 +12,33 @@ check_installed() {
 }
 
 setup_python_environment() {
-	check_installed "python3"
-	check_installed "python3-venv"
-	check_installed "python3-pip"
+	if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+		cat <<- EOF
+
+		***************************
+		*    This script is not   *
+		*    being sourced. venv  *
+		*    will not enter       *
+		*    correctly. Please,   *
+		*    run it as            *
+		*                         *
+		*    source ./setup.sh    *
+		*                         *
+		*    instead of           *
+		*                         *
+		*    ./setup.sh           *
+		*                         *
+		***************************
+
+		EOF
+		exit 1
+	else
+		echo "Script is being sourced. Continuing..."
+		check_installed "python3"
+		check_installed "python3-venv"
+		check_installed "python3-pip"
+
+	fi
 
 	echo "Creating virtual environment..."
 	python3 -m venv venv
@@ -22,9 +46,10 @@ setup_python_environment() {
 	source venv/bin/activate
 
 	echo -e "\nPython venv configured successfully.\n"
+	echo -e "Use the deactivate command to leave it.\n"
 }
 
-cat << EOF
+cat <<- EOF
 
 **************** SETUP ****************
 *                                     *
